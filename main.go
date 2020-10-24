7b4efeb05b495284e1363e578f99f7614edfe390
easygogo <quanlongwang@gmail.com>
2020-10-24 23:32:00 +0800

    完善代码

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
 )
 
 var addr = flag.String("addr", ":8080", "http server address")
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
 	wsServer := NewWebsocketServer()
 	go wsServer.Run()
 
-    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-        ServeWs(w, r)
-    })
-    
-    fs := http.FileServer(http.Dir("./public"))
-    http.Handle("/", fs)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		ServeWs(wsServer, w, r)
+	})
 
-    log.Fatal(http.ListenAndServe(*addr, nil))
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
+
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
